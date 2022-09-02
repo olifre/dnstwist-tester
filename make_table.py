632d8c8fc53eae96dbd0e2fa1f38f4e67479846a
Oliver Freyermuth <o.freyermuth@googlemail.com>
2022-09-02 03:51:49 +0200

    make_table: Allow to filter out unstable fields.

--- make_table.py
+++ make_table.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 import csv
 import sys
+import argparse
 
-if len(sys.argv) != 2:
-  print("Need exactly 1 commandline argument, got {} instead".format(len(sys.argv)-1))
-  sys.exit(1)
+unstable_fields = [ 'ssdeep', 'banner_http', 'banner_smtp' ]
 
-with open(sys.argv[1], 'rt') as csvfile:
+argparser = argparse.ArgumentParser(description='CSV to HTML table formatting tool for dnstwist output.')
+
+argparser.add_argument('-f', '--filter_unstable',
+    help="Filters out unstable fields",
+    action="store_true", dest="filter_unstable", default=False,
+)
+argparser.add_argument('csvfile',
+    help="CSV file to read",
+    action="store",
+)
+
+args = argparser.parse_args()
+
+with open(args.csvfile, 'rt') as csvfile:
   reader = csv.reader(csvfile, quotechar='"')
   total_rows = sum(1 for row in reader)
   csvfile.seek(0)
@@ -14,15 +26,15 @@ with open(sys.argv[1], 'rt') as csvfile:
   for rowc, row in enumerate(reader):
     if (rowc == 0):
       print("<thead>")
+      coldesc = row
     elif (rowc == 1):
       print("<tbody>")
     print("<tr>", end="")
-    if (rowc == 0):
-      for col in row:
-        print("<th>{}</th>".format(col), end="")
-    else:
-      for col in row:
-        print("<td>{}</td>".format(col), end="")
+    td_type = 'td' if (rowc > 0) else 'th'
+    for colidx, col in enumerate(row):
+      if args.filter_unstable and (coldesc[colidx] in unstable_fields):
+        continue
+      print("<{}>{}</{}>".format(td_type, col, td_type), end="")
     print("</tr>")
     if (rowc == 0):
       print("</thead>")
